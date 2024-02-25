f70676125153c6350d78c82fe44e3ef6d61fedc9
g1ammi93 <giammariacarelli93@hotmail.it>
2024-02-25 21:42:46 +0000

    Added New Task

--- js/script.js
+++ js/script.js
@@ -4,8 +4,20 @@ const endpoint = 'http://localhost/Boolean/php-todo-list-json/tasks/';
 
 const app = createApp({
     data: () => ({
-        tasks: []
+        tasks: [],
+        newTask: ''
     }),
+    methods: {
+        addTask() {
+            const data = { task: this.newTask }
+            const config = { headers: { 'Content-Type': 'multipart/form-data' } }
+
+            axios.post(endpoint, data, config).then(res => {
+                this.tasks = res.data;
+                this.newTask = '';
+            })
+        }
+    },
     created() {
         axios.get(endpoint).then(res => {
             this.tasks = res.data;
--- tasks/index.php
+++ tasks/index.php
@@ -1,28 +1,27 @@
 <?php
+header('Content-Type: application/json');
 
-$tasks = [
-    [
-      "id"=> 1,
-      "text"=> "HTML",
-      "done"=> true
-    ],
-    [
-      "id"=> 2,
-      "text"=> "CSS",
-      "done"=> true
-    ],
-    [
-      "id"=> 3,
-      "text"=> "Responsive design",
-      "done"=> true
-    ],
-    [
-      "id"=> 4,
-      "text"=> "Javascript",
-      "done"=> true
-    ]
+$source_path = __DIR__ . '/../database/tasks.json';
+
+$json_data = file_get_contents($source_path);
+
+$tasks = json_decode($json_data, true);
+
+$new_task_text = $_POST['task'] ?? '';
+
+
+if($new_task_text) {
+$new_task = [
+    'id' => count($tasks) +1,
+    'text' => $new_task_text,
+    'done' => false
 ];
 
-header('Content-Type: application/json');
+$tasks[] = $new_task;
+
+file_put_contents($source_path, json_encode($tasks, JSON_PRETTY_PRINT));
+}
+
+
 
 echo json_encode($tasks);
