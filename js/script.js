f39471d431d0a4031e49ba96886b7c39ac911429
g1ammi93 <giammariacarelli93@hotmail.it>
2024-02-25 20:34:06 +0000

    Added Html and CSS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,30 @@
 const { createApp } = Vue;
 
 const app = createApp({
-
+    data: () => ({
+        task: [
+            {
+                "id": 1,
+                "text": "HTML",
+                "done": true
+            },
+            {
+                "id": 2,
+                "text": "CSS",
+                "done": true
+            },
+            {
+                "id": 3,
+                "text": "Responsive design",
+                "done": true
+            },
+            {
+                "id": 4,
+                "text": "Javascript",
+                "done": true
+            }
+        ]
+    })
 
 })
 
